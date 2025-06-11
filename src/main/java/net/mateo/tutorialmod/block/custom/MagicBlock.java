0ec276cf5e493e324236bef1d2dfc09891f0b50b
CYP3RBOT <CYP3RBOT8009@gmail.com>
2025-06-11 08:44:59 -0400

    Push code

--- src/main/java/net/mateo/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/mateo/tutorialmod/block/custom/MagicBlock.java
@@ -1,25 +1,19 @@
 package com.mateo.tutorialmod.block.custom;
 
 import com.mateo.tutorialmod.item.ModItems;
-import com.mateo.tutorialmod.util.ModTags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-
-import java.util.List;
 
 public class MagicBlock extends Block {
     public MagicBlock(Properties properties) {
@@ -36,7 +30,7 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
         if(pEntity instanceof ItemEntity itemEntity) {
-            if(isValidItem(itemEntity.getItem())) {
+            if(itemEntity.getItem().getItem() == ModItems.RAW_ALEXANDRITE.get()) {
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
 
@@ -46,15 +40,5 @@ public class MagicBlock extends Block {
         }
 
         super.stepOn(pLevel, pPos, pState, pEntity);
-    }
-
-    private boolean isValidItem(ItemStack item) {
-        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
-    }
-
-    @Override
-    public void appendHoverText(ItemStack pStack, Item.TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
-        pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.magic_block.tooltip"));
-        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
 }
